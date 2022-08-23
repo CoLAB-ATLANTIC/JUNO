5fd8468ce159be4ded598c4eaeae968d486f8900
Luis Figueiredo <Luis.figueiredo@colabatlantic.com>
2022-08-23 17:19:05 +0100

    create netCDF with data of CCA MUR front arrays

--- src/front_arrays.sh
+++ src/front_arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-d=20120101
-while [ "$d" != 20220101 ]; do
+d=20190601
+while [ "$d" != 20190611 ]; do
   echo $d
   python JUNO/src/CCA_frontal_prob_xarray.py 'sst_'"$d"'.nc'
   d=$(date +%Y%m%d -d "$d + 1 day")
---
+++ src/front_arrays2NetCDF.py
@@ -0,0 +1,61 @@
+
+#Import Libraries
+
+import numpy as np
+import os
+import netCDF4 as nc
+import glob
+import datetime
+
+
+nc_file = os.getcwd()
+nc_file = os.path.join('JUNO/data/CCA_MUR_fronts.nc')
+
+
+ds = nc.Dataset(nc_file, 'w', format='NETCDF4')
+
+ds.title = 'CCA MUR Fronts Arrays'
+
+time = ds.createDimension('time')
+lat = ds.createDimension('lat', 1001)
+lon = ds.createDimension('lon', 1401)
+
+times = ds.createVariable('time', np.int32, ('time', ))
+lats = ds.createVariable('lat', 'f4', ('lat', ))
+lons = ds.createVariable('lon', 'f4', ('lon', ))
+value = ds.createVariable('value', 'u1', ('time', 'lat', 'lon',))
+value.units = 'Unknown'
+times.units = 'days since 01-01-01'
+
+lats[:] = np.linspace(35, 45, 1001)
+lons[:] = np.linspace(-19, -5, 1401)
+
+base_path = os.getcwd()
+base_path = os.path.join('JUNO')
+
+count = 0    
+dates = []
+
+for filename in sorted(glob.glob((os.path.join(base_path, 'data/MUR_daily_fronts_npy/*.npy')))):
+    
+    date_str = filename.split('sst_', 1)[1]
+    date_str = date_str.split('.', 1)[0]
+
+    
+    date_obj = datetime.datetime.strptime(date_str, '%Y%m%d')
+    dates.append(date_obj)
+    
+    value[count, :, :] = np.load(filename)
+    
+    count += 1
+    
+#times = dates
+dates_time = [tmp.toordinal() for tmp in dates]
+#dates_time = date2num(dates, times.units)
+times[:] = dates_time
+
+
+ds.close()
+
+
+
