6c6d7071e8d6b6acd852fa752ea140f43569c10e
Luis Figueiredo <Luis.figueiredo@colabatlantic.com>
2022-09-14 13:54:00 +0100

    test script using Xarrays instead of dataframes 4

--- src/test.py
+++ src/test.py
@@ -108,7 +108,7 @@ def get_data(data, base_path):
     """
     
     base_path = base_path
-    data_folder = os.path.join(base_path, "data/MUR_single_days")  
+    data_folder = os.path.join(base_path, "data/MUR_daily_data")  
     
     nc_path = os.path.join(data_folder, data)
     data_xarray = xr.load_dataset(nc_path)
