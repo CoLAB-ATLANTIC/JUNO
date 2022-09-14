b414703ced0800546756defbfbfd5d85252b6df0
Luis Figueiredo <Luis.figueiredo@colabatlantic.com>
2022-09-14 12:10:05 +0100

    test script using Xarrays instead of dataframes

--- src/MUR_dailyFronts2NetCDF.py
+++ src/MUR_dailyFronts2NetCDF.py
@@ -1,6 +1,3 @@
-##### VOU TENTAR IMPLEMENTAR O MUR_dailyFronts2NetCDF.py MAS SEM USAR DATAFRAMES (XARRAYS)
-
-
 
 # Script which, through a daily cron job makes the donwload of daily MUR data and saves it in the MUR_daily_data folder.
 # Then it applies the 3 algorithms to the data and stores the result arrays in a netCDF file
@@ -24,7 +21,7 @@ import netCDF4 as nc
 matplotlib.use('Agg')    #por causa do erro AttributeError: 'NoneType' object has no attribute 'set_cursor'
 
 import BOA
-import CayulaCornillon_xarray
+import CayulaCornillon_df
 
 
 ################################################ DOWNLOAD MUR DATA ##################################################
@@ -100,28 +97,36 @@ def download_sst(path, date, mur_j0, mur_j1, mur_i0, mur_i1, replace):
 
 ######################################### IMPORT DATA #######################################################
 
-def get_data(base_path, data):
+def get_data(data, base_path):
     
     """
-    Function to get our netCDF file that is stored in the data directory inside the MUR_seasonal_data folder and convert it to a dataframe.
+    Function to get our netCDF file that is stored in the data directory and convert it to a dataframe.
     The data parameter is the string name of the netCDF file we want to import
     """
     
     base_path = base_path
-    data_folder = os.path.join(base_path, "data/MUR_single_days")  
+    data_folder = os.path.join(base_path, "data/MUR_daily_data")           
     
     nc_path = os.path.join(data_folder, data)
-    data_xarray = xr.load_dataset(nc_path)
+    netCDF = xr.load_dataset(nc_path)
     
-    #In the case the temperature is in Kelvin (Convert to Celsius)
-    data_xarray['analysed_sst'] = data_xarray['analysed_sst'] - 273.15
+    df = netCDF.to_dataframe()
+    df = df.reset_index()
     
-    return data_xarray
+    df = df.drop(['depth'], axis=1, errors='ignore') #drop the column 'depth' if exists: only exists in reanalysis
+    
+    #if we are importing MUR data, rename columns and convert temperature to Celsius
+    if data.startswith('mur') or data.startswith('sst'):   #the downloaded individual day mur data name starts with sst
+        df.rename(columns={'lat': 'latitude', 'lon': 'longitude', 'time': 'time', 'analysed_sst':'thetao'}, inplace=True)
+        df['thetao'] = df['thetao']-273.15   
+        
+    
+    return df
 
 
 #################################### CANNY ALGORITHM ##########################################################
 
-def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, apertureSize=5, sigma=5):
+def canny_front_detection_1day(df, thresh_min=120, thresh_max=220, apertureSize=5, sigma=5):
     
     """
     This function receives a dataframe with MUR data for a individual day and returns the array 
@@ -129,10 +134,9 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
     For visualization purposes, one can change the minimum and maximum threshold.
     One can also apply a gaussian filter with a certain sigma value to reduce noise of the image.
     """
-    
-    #Get the sst array in the right shape
-    sst = np.array(data_xarray['analysed_sst'])
-    sst = np.squeeze(sst)
+
+    # Convert the df to a numpy array with the SST values for the coordinate pair (longitude and latitude)
+    sst = df.pivot_table(index='longitude', columns='latitude', values='thetao').T.values
     #Convert Temperature values to uint8 format with values in the range of 0-255
     sst_final = ((sst - np.nanmin(sst)) * (1/(np.nanmax(sst) - np.nanmin(sst)) * 255)).astype('uint8')
     sst_final = np.flipud(sst_final)   #flipud -> Reverse the order of elements along axis 0 (up/down).
@@ -145,6 +149,8 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
     
     canny[canny == 255] = 1
     
+    
+
     #Apply a mask for the continental zone:
     mask = np.isnan(np.flipud(sst))    #Boolean array: True where array Temp had Null Values (correspond to the continental zone)
     mask255 =np.where(mask,(np.ones(mask.shape))*255,0).astype("uint8")   #array which values= 255 when mask=True
@@ -153,7 +159,8 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
     mask_dilated = cv2.dilate(mask255, kernel)
     canny_front = np.ma.masked_array(canny, mask_dilated)   #Mask an array where a condition is True
     
-    canny_front = np.flipud(canny_front)    
+    
+    canny_front = np.flipud(canny_front) 
     
     
     return canny_front
@@ -166,7 +173,7 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
 
 
 
-def BOA_aplication(data_xarray, threshold = 0.05):  
+def BOA_aplication(df, threshold = 0.05):  
     
     """
     Function to, for a given dataframe with a longitude, latitude and SST columns, 
@@ -175,11 +182,9 @@ def BOA_aplication(data_xarray, threshold = 0.05):
     (if the pixel value is greater than the threshold, then it is considered a front, otherwise don't). 
     """
     
-    lon = np.array(data_xarray['lon']).astype('float64')
-    lat = np.array(data_xarray['lat']).astype('float64')
-    ingrid = np.array(data_xarray['analysed_sst'])
-    ingrid = np.squeeze(ingrid)
-    
+    lat = np.array(df['latitude'].unique())
+    lon = np.array(df['longitude'].unique())
+    ingrid = np.array(df['thetao']).reshape(len(lat), len(lon))
     
     boa_front = BOA.boa(lon=lon, lat=lat, ingrid=ingrid, nodata = np.nan, direction = False)
     boa_front = np.flip(boa_front, axis=0)
@@ -191,10 +196,9 @@ def BOA_aplication(data_xarray, threshold = 0.05):
     
     
     #Create a masked_array in order to get the continental zone well defined
-    #sst = np.array(data_xarray['analysed_sst'])
-    #sst = np.squeeze(sst)    #--> Em vez disto posso simplesmente usar o ingrid criado em cima
-
-    mask = np.isnan(np.flipud(ingrid))       #Boolean array=True where array Temp had Null values (continental zone)
+    #Convert some df to a numpy array with the SST values for each value of longitude and latitude
+    sst = df.pivot_table(index='longitude', columns='latitude', values='thetao').T.values   
+    mask = np.isnan(np.flipud(sst))       #Boolean array=True where array Temp had Null values (continental zone)
     mask255 =np.where(mask,(np.ones(mask.shape))*255,0).astype("uint8")   #array which pixels = 255 when mask=True 
     #Make a dilation to ensure the pixels that belong to the shore are not consideredd fronts
     kernel = np.ones((3,3), np.uint8)
@@ -210,51 +214,41 @@ def BOA_aplication(data_xarray, threshold = 0.05):
 ################################## CAYULA-CORNILLON ALGORITHM #################################################################
     
 
-    
-def CCA_front(data_xarray): 
+def CCA_front(df): 
     
     """
-    Function that calculates the fronts matrix. Given an image (SST data respective to one day) it applies the
-    Cayula-Cornillon Algorithm for Single Image Edge Detection (CCA-SIED) to discover the fronts.
-    It returns the matrix with the fronts: if pixel = 1 it was considered a front, otherwise, pixel = 0
-    It basically converts the (x,y) coordinate points to indexes of the frontal probability matrix. These indexes are considered fronts
-    The df parameter is the dataframe with the SST data for a certain day
+    This function receives a dataframe with MUR data for a individual day and plots the result
+    of the aplication of the Cayula-Cornillon Algorithm
     """
     
+
     front = np.zeros((1001,1401))       #initialize a matrix of zeros. This shape is for the MUR data
+        
+    xdata_final, ydata_final = CayulaCornillon_df.CCA_SIED(df, shape=(1001, 1401))       
     
-    #2 empty arrays that will store the x and y values of the lines that are suposed to be drawn
-    x = np.array([])
-    y = np.array([])
-        
-    xdata_final, ydata_final = CayulaCornillon_xarray.CCA_SIED(data_xarray)       
-    x = np.append(x, xdata_final)
-    y = np.append(y, ydata_final)
-        
     cols_x = np.array([])
-    for value in x:                     #convert values in array x to the respective index in a (1001, 1401) matrix
+    for value in xdata_final:                     #convert values in array x to the respective index in a (1001, 1401) matrix
         aux_x = (19+value)/0.01                  #these numbers are relative to the MUR data
         cols_x = np.append(cols_x, aux_x)
     
     rows_y = np.array([])
-    for value in y:                     #convert values in array y to the respective index in a (1001, 1401) matrix
+    for value in ydata_final:                     #convert values in array y to the respective index in a (1001, 1401) matrix
         aux_y = (45-value)/0.01                  #these numbers are relative to the MUR data
         rows_y = np.append(rows_y, aux_y)
-     
+
     cols_x = np.round(cols_x)
     rows_y = np.round(rows_y)
             
     for i in range(len(cols_x)):   #it could also be len(rows_y)
         front[int(rows_y[i]), int(cols_x[i])] = front[int(rows_y[i]), int(cols_x[i])] + 1
-            
+        
     front[front != 0] = 1
-
-
+    
+    
     #Create a masked_array in order to get the continental zone well defined
     
     #Convert some df to a numpy array with the SST values for each value of longitude and latitude
-    sst = np.array(data_xarray['analysed_sst'])
-    sst = np.squeeze(sst)
+    sst = df.pivot_table(index='longitude', columns='latitude', values='thetao').T.values   
     mask = np.isnan(np.flipud(sst))       #Boolean array=True where array Temp had Null values (continental zone)
     mask255 =np.where(mask,(np.ones(mask.shape))*255,0).astype("uint8")   #array which pixels = 255 when mask=True 
     #Make a dilation to ensure the pixels that belong to the shore are not consideredd fronts
@@ -263,6 +257,8 @@ def CCA_front(data_xarray):
     cca_front = np.ma.masked_array(front, mask_dilated)  
     
     cca_front = np.flipud(cca_front) 
+    
+    #cca_front[cca_front == 255] = 1
     
     return cca_front
     
@@ -278,6 +274,7 @@ def real_sst_image(df):
     """
     
     sst = df.pivot_table(index='longitude', columns='latitude', values='thetao').T.values
+    #sst = np.flipud(sst)
     
     return sst
  
@@ -287,6 +284,7 @@ def main():
     
     base_path = os.getcwd()
     base_path = os.path.join(base_path, 'projects/JUNO')      #servidor
+   # base_path = os.path.join(base_path, 'JUNO')               #minha maquina
     
     #download MUR data for the day before yesterday
     day_txt = (date.today() - timedelta(days=2)).strftime('%Y%m%d')
@@ -320,7 +318,7 @@ def main():
 
     ds = nc.Dataset(nc_file, 'w', format='NETCDF4')
 
-    ds.title = 'MUR ' + day_txt + ' Fronts Arrays (Xarrays)'
+    ds.title = 'MUR ' + day_txt + ' Fronts Arrays (Dataframe)'
 
     #create dimensions of the NetCDF file
     time = ds.createDimension('time')
