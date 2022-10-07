31d1ac30099594ec2250750bd03d2843598b8998
Luis Figueiredo <Luis.figueiredo@colabatlantic.com>
2022-10-07 16:10:35 +0100

    convert 0s to Nans in MUR cronjob script 2

--- src/MUR_daily_fronts_netcdf.py
+++ src/MUR_daily_fronts_netcdf.py
@@ -138,8 +138,9 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
     """
     
     #Get the sst array in the right shape
-    sst = np.array(data_xarray['analysed_sst'])
-    sst = np.squeeze(sst)
+    sst = data_xarray['analysed_sst'][0,:,:].values
+    #sst = np.array(data_xarray['analysed_sst'])
+    #sst = np.squeeze(sst)
     #Convert Temperature values to uint8 format with values in the range of 0-255
     sst_final = ((sst - np.nanmin(sst)) * (1/(np.nanmax(sst) - np.nanmin(sst)) * 255)).astype('uint8')
     sst_final = np.flipud(sst_final)   #flipud -> Reverse the order of elements along axis 0 (up/down).
@@ -152,6 +153,9 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
     
     canny[canny == 255] = 1
     
+    #convert 0s to Nans
+    canny[canny == 0] = 'nan'
+    
     #Apply a mask for the continental zone:
     mask = np.isnan(np.flipud(sst))    #Boolean array: True where array Temp had Null Values (correspond to the continental zone)
     mask255 =np.where(mask,(np.ones(mask.shape))*255,0).astype("uint8")   #array which values= 255 when mask=True
@@ -161,9 +165,6 @@ def canny_front_detection_1day(data_xarray, thresh_min=120, thresh_max=220, aper
     canny_front = np.ma.masked_array(canny, mask_dilated)   #Mask an array where a condition is True
     
     canny_front = np.flipud(canny_front)    
-    
-    #convert 0s to Nans
-    canny_front[canny_front == 0] = 'nan'
     
     
     return canny_front
@@ -187,8 +188,10 @@ def BOA_aplication(data_xarray, threshold = 0.05):
     
     lon = np.array(data_xarray['lon']).astype('float64')
     lat = np.array(data_xarray['lat']).astype('float64')
-    ingrid = np.array(data_xarray['analysed_sst'])
-    ingrid = np.squeeze(ingrid)
+    
+    ingrid = data_xarray['analysed_sst'][0,:,:].values
+   # ingrid = np.array(data_xarray['analysed_sst'])
+    #ingrid = np.squeeze(ingrid)
     
     
     boa_front = BOA.boa(lon=lon, lat=lat, ingrid=ingrid, nodata = np.nan, direction = False)
@@ -197,14 +200,18 @@ def BOA_aplication(data_xarray, threshold = 0.05):
     
     boa_front = np.where(boa_front>=threshold, 1, boa_front)    
     boa_front = np.where(boa_front<threshold, 0, boa_front)
+    
+    #convert 0s to Nans
+    boa_front[boa_front == 0] = 'nan'
 
     
     
     #Create a masked_array in order to get the continental zone well defined
     #sst = np.array(data_xarray['analysed_sst'])
     #sst = np.squeeze(sst)    #--> Em vez disto posso simplesmente usar o ingrid criado em cima
+    sst = data_xarray['analysed_sst'][0,:,:].values
 
-    mask = np.isnan(np.flipud(ingrid))       #Boolean array=True where array Temp had Null values (continental zone)
+    mask = np.isnan(np.flipud(sst))       #Boolean array=True where array Temp had Null values (continental zone)
     mask255 =np.where(mask,(np.ones(mask.shape))*255,0).astype("uint8")   #array which pixels = 255 when mask=True 
     #Make a dilation to ensure the pixels that belong to the shore are not consideredd fronts
     kernel = np.ones((3,3), np.uint8)
@@ -214,7 +221,7 @@ def BOA_aplication(data_xarray, threshold = 0.05):
     boa_front = np.flipud(boa_front) 
     
     #convert 0s to Nans
-    boa_front[boa_front == 0] = 'nan'
+    #boa_front[boa_front == 0] = 'nan'
 
     return boa_front
 
@@ -261,13 +268,18 @@ def CCA_front(data_xarray):
         front[int(rows_y[i]), int(cols_x[i])] = front[int(rows_y[i]), int(cols_x[i])] + 1
             
     front[front != 0] = 1
+    
+    #convert 0s to Nans
+    front[front == 0] = 'nan'
 
 
     #Create a masked_array in order to get the continental zone well defined
     
     #Convert some df to a numpy array with the SST values for each value of longitude and latitude
-    sst = np.array(data_xarray['analysed_sst'])
-    sst = np.squeeze(sst)
+    #sst = np.array(data_xarray['analysed_sst'])
+    #sst = np.squeeze(sst)
+    sst = data_xarray['analysed_sst'][0,:,:].values
+    
     mask = np.isnan(np.flipud(sst))       #Boolean array=True where array Temp had Null values (continental zone)
     mask255 =np.where(mask,(np.ones(mask.shape))*255,0).astype("uint8")   #array which pixels = 255 when mask=True 
     #Make a dilation to ensure the pixels that belong to the shore are not consideredd fronts
@@ -278,7 +290,7 @@ def CCA_front(data_xarray):
     cca_front = np.flipud(cca_front) 
     
     #convert 0s to Nans
-    cca_front[cca_front == 0] = 'nan'
+    #cca_front[cca_front == 0] = 'nan'
     
     return cca_front
     
@@ -318,7 +330,6 @@ def main():
     
     xarray_mur = get_data(data = 'sst_' + day_txt + '.nc', base_path=base_path)     #convert the netcdf with MUR data to a dataframe to later apply the algorithms
     
-    #sst = real_sst_image(xarray_mur)
     
     canny_front = canny_front_detection_1day(xarray_mur)
     
