94f1bbdfd650a90f1e2f4e6d13bb4c0780d50b14
Luis Figueiredo <Luis.figueiredo@colabatlantic.com>
2022-10-10 14:12:54 +0100

    try diferent parameters of BOA and Canny in CMEMS 2

--- src/CMEMS_daily_fronts_netcdf.py
+++ src/CMEMS_daily_fronts_netcdf.py
@@ -90,7 +90,7 @@ def get_data(data, base_path):
 
 #################################### CANNY ALGORITHM ##########################################################
 
-def canny_front_detection_1day(data_xarray, thresh_min=140, thresh_max=220, apertureSize=3, sigma=5):
+def canny_front_detection_1day(data_xarray, thresh_min=140, thresh_max=230, apertureSize=5, sigma=3):
     
     """
     This function receives a dataframe with CMEMS Forecast data for a individual day and returns the array 
@@ -132,7 +132,7 @@ def canny_front_detection_1day(data_xarray, thresh_min=140, thresh_max=220, aper
 ################################### Belkin O'Reilly Algorithm ##########################################################
 
 
-def BOA_aplication(data_xarray, threshold = 0.1):  
+def BOA_aplication(data_xarray, threshold = 0.2):  
     
     """
     Function to, for a given dataframe with a longitude, latitude and SST columns, 
